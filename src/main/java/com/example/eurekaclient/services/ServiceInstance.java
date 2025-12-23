3443f36df2ac8d7719739848bad0031d99aa15d6
wlanboy <wlanboy@github.com>
2025-12-23 08:45:09 +0100

    Lombock refactoring

--- src/main/java/com/example/eurekaclient/services/ServiceInstance.java
+++ src/main/java/com/example/eurekaclient/services/ServiceInstance.java
@@ -1,6 +1,18 @@
 package com.example.eurekaclient.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class ServiceInstance {
+
     private Long id;
 
     private String serviceName;
@@ -11,77 +23,4 @@ public class ServiceInstance {
     private String dataCenterInfoName;
     private String status;
     private boolean sslPreferred;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
-    }
-
-    public String getHostName() {
-        return hostName;
-    }
-
-    public void setHostName(String hostName) {
-        this.hostName = hostName;
-    }
-
-    public int getHttpPort() {
-        return httpPort;
-    }
-
-    public void setHttpPort(int httpPort) {
-        this.httpPort = httpPort;
-    }
-
-    public int getSecurePort() {
-        return securePort;
-    }
-
-    public void setSecurePort(int securePort) {
-        this.securePort = securePort;
-    }
-
-    public String getDataCenterInfoName() {
-        return dataCenterInfoName;
-    }
-
-    public void setDataCenterInfoName(String dataCenterInfoName) {
-        this.dataCenterInfoName = dataCenterInfoName;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    
-    public String getIpAddr() {
-        return ipAddr;
-    }
-
-    public void setIpAddr(String ipAddr) {
-        this.ipAddr = ipAddr;
-    }
-
-    public boolean isSslPreferred() {
-        return sslPreferred;
-    }
-
-    public void setSslPreferred(boolean sslPreferred) {
-        this.sslPreferred = sslPreferred;
-    }
 }
