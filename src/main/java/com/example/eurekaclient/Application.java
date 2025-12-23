343b80c7510152b62defa6cf4692982906ed40d3
wlanboy <wlanboy@github.com>
2025-12-23 08:44:12 +0100

    Lombock refactoring

--- src/main/java/com/example/eurekaclient/Application.java
+++ src/main/java/com/example/eurekaclient/Application.java
@@ -1,31 +1,20 @@
 package com.example.eurekaclient;
 
+import com.example.eurekaclient.services.LifecycleManager;
+import com.example.eurekaclient.services.ServiceInstanceStore;
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.eurekaclient.services.LifecycleManager;
-import com.example.eurekaclient.services.ServiceInstance;
-import com.example.eurekaclient.services.ServiceInstanceStore;
-
-import jakarta.annotation.PreDestroy;
-
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+@Slf4j
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
-
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     private final ServiceInstanceStore store;
     private final LifecycleManager lifecycleManager;
-
-    public Application(ServiceInstanceStore store, LifecycleManager lifecycleManager) {
-        this.store = store;
-        this.lifecycleManager = lifecycleManager;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -35,8 +24,10 @@ public class Application {
     @PreDestroy
     public void onShutdown() {
         log.info(">>> Anwendung wird heruntergefahren – stoppe alle Eureka Clients...");
-        List<ServiceInstance> allInstances = store.getInstances();
-        lifecycleManager.stopAll(allInstances);
+
+        lifecycleManager.stopAll(store.getInstances());
+
         log.info(">>> Alle Clients gestoppt und deregistriert.");
     }
+
 }
